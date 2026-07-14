810f87f13c31b90cc5db7965af890a357eef8f9d
MD Anik Hasan <anikhasan2@icloud.com>
2026-07-14 22:07:38 +0600

    Fix remaining workspace diagnostics

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,12 +1,4 @@
-import 'react'
-
 declare module '*.css'
 declare module '*.scss'
 declare module '*.module.css'
 declare module '*.module.scss'
-
-declare module 'react' {
-  interface HTMLAttributes<T> {
-    tw?: string
-  }
-}
